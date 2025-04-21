0ae23a69c08b5548c6941d37a79a9f359ffd9ceb
José Guilherme <joseguilhermef11@gmail.com>
2025-04-21 13:22:19 +0000

    Commit Nivel Novato

--- xadrez.c
+++ xadrez.c
@@ -5,17 +5,42 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+    printf("---------------------------------------\n");
+    printf("DESAFIO XADREZ - MOVIMENTAÇÃO DAS PEÇAS\n");
+    printf("---------------------------------------\n");
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    // Declaranfo variáveis
+    int mov_bispo = 1, mov_torre = 1, mov_rainha = 1;
+    int limite_bispo = 5, limite_torre = 5, limite_rainha = 8;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Movimentando Bispo
+    // Utilizando While para o Movimento do Bispo cinco casas na Diagonal
+    printf(">> Movimento do BISPO:\n");
+    while (mov_bispo <= limite_bispo) {
+        printf("Cima Direita\n");
+        mov_bispo++;
+    }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("---------------------------------------\n");
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Movimentando Torre
+    // Utilizando For para o Movimento da Torre cinco casas para a Direita
+    printf(">> Movimento da TORRE:\n");
+    for (limite_torre = 5; mov_torre <= limite_torre; mov_torre++) {
+        printf("Direita\n");
+    }
+
+    printf("---------------------------------------\n");
+
+    // Movimentando Rainha
+    // Utilizando Do-While para o Movimento da Rainha oito casas para a Esquerda
+    printf(">> Movimento da RAINHA:\n");
+    do {
+        printf("Esquerda\n");
+        mov_rainha++;
+    } while (mov_rainha <= limite_rainha);
+
+    printf("---------------------------------------\n");
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
